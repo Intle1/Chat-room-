3514dfb08ed49e3f8e6660f2b5b0d5ed423459ee
Intle1 <johnsonjrquintonm@gmail.com>
2025-04-18 08:59:33 -0400

    Update Server.js

--- Server.js
+++ Server.js
@@ -1,9 +1,5 @@
-const dns = require('dns');
 const express = require('express');
 const path = require('path');
-
-// Set custom DNS servers
-dns.setServers(['8.8.8.8', '1.0.0.1']);
 
 const app = express();
 
@@ -12,9 +8,8 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Start the server
+// Start the server on port 8080
 const PORT = 8080;
 app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server is running on http://10.11.128.1:${PORT}`);
-    console.log(`Using DNS servers: ${dns.getServers().join(', ')}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
